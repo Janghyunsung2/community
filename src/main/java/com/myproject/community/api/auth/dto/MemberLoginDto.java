2d867a7420ecb4ee70c524592b89cbc236870b61
Janghyunsung2 <tlswlfk@gmail.com>
2025-03-06 14:54:39 +0900

    test/authService : 단위 테스트코드 작성

--- src/main/java/com/myproject/community/api/auth/dto/MemberLoginDto.java
+++ src/main/java/com/myproject/community/api/auth/dto/MemberLoginDto.java
@@ -2,12 +2,14 @@ package com.myproject.community.api.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class MemberLoginDto {
 
     @NotBlank
--- src/main/java/com/myproject/community/api/member/dto/MemberResponseDto.java
+++ src/main/java/com/myproject/community/api/member/dto/MemberResponseDto.java
@@ -1,12 +1,14 @@
 package com.myproject.community.api.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MemberResponseDto {
 
     private String name;
